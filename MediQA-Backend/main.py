4c141ede351182746d8ea130013296f004dad059
ZyKaiyZ <yoyo8117431@gmail.com>
2023-10-31 00:53:39 +0800

    Feat: Update dockerfile of backend

--- MediQA-Backend/main.py
+++ MediQA-Backend/main.py
@@ -42,4 +42,4 @@ async def ask_question(question_input: QuestionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
